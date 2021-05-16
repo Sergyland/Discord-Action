22ec09bcb1e301e037dabef19a4e7fd68c99684e
serge55555 <serge55555@hotmail.fr>
2021-05-16 03:32:05 +0200

    Modfified

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const core = require('@actions/core');
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN)
+client.login(core.getInput("token"))
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const botchannel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
-    core.info(`Connected to channel!`);
-    botchannel.send("Send from github!");
-    botchannel.send(process.env);
+    const botchannel = await client.channels.fetch(core.getInput("channel"));
+    botchannel.send("Sent from github!");
   } catch (error) {
     core.setFailed(error.message);
+    process.exit(1)
   }
 }
 
