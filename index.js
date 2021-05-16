a8962ae7b79f450ff9dac0d0b88c918e9ec8323a
serge55555 <serge55555@hotmail.fr>
2021-05-16 03:43:43 +0200

    Changed to event listening

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 client.login(core.getInput("token"))
+var botchannel;
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const botchannel = await client.channels.fetch(core.getInput("channel"));
-    botchannel.send("Sent from github!");
-  } catch (error) {
-    core.setFailed(error.message);
-    process.exit(1)
-  }
-}
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  /* TODO : Add bot activity
 
-run();
+  */
+    try {
+        botchannel = await client.channels.fetch(core.getInput("channel"))
+    } catch(e) {
+        console.error("An error occured during channel access atempt.",e)
+    }
+    console.log("Connected to channel!");
+    try {
+        botchannel.send("Sent from github!")
+    } catch(e) {
+        console.error("An error occured during message sending.")
+    }
+    process.exit(0)
+});
